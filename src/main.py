190031ac8b670670fd2e0349686b6ee92f1604f6
Jaime Burnap <jaime@burnap.io>
2019-10-08 19:35:54 -0700

    Logging updates

--- src/main.py
+++ src/main.py
@@ -93,6 +93,17 @@ alexa_logger.setLevel(log_level)
 
 logger = logging.getLogger(__name__)
 
+logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
+rootLogger = logging.getLogger()
+
+fileHandler = logging.FileHandler("{0}/{1}.log".format("./", "alexa"))
+fileHandler.setFormatter(logFormatter)
+rootLogger.addHandler(fileHandler)
+
+# consoleHandler = logging.StreamHandler()
+# consoleHandler.setFormatter(logFormatter)
+# rootLogger.addHandler(consoleHandler)
+
 if not config_exists:
     logger.critical('Can not find configuration file. Exiting...')
     sys.exit(1)
